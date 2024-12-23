3557385e9e6f2d0262e46723cf0ca763b240e345
rihab-nikh <rihab_nikh@outlook.fr>
2024-12-23 21:02:27 +0100

    register login work and it redirects to Dashboard

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,13 @@
+import { enableProdMode } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { importProvidersFrom } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AppRoutingModule } from './app/app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+bootstrapApplication(AppComponent, {
+  providers: [
+    importProvidersFrom(CommonModule, AppRoutingModule, HttpClientModule)
+  ]
+}).catch(err => console.error(err));
